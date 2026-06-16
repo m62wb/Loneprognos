e53de37b24f30792ec080ec786c7b6d8243b3ca0
m62wb <m62wbvczjg@privaterelay.appleid.com>
2026-06-16 16:50:09 +0200

    Add null check before input event listener

--- js/script.js
+++ js/script.js
@@ -431,16 +431,18 @@ window.sgiInput = sgiInput;
 window.obLockToggle = obLockToggle;
 
 document.querySelectorAll('.numeric-only').forEach(function(field) {
-  field.addEventListener('input', function() {
-    this.value = this.value.replace(/[^0-9.,]/g, '');
-    if (this.value.includes(',')) {
-      this.value = this.value.replace(',', '.');
-    }
-    if (field.classList.contains('numeric-hours')) {
-      var match = this.value.match(/^(\d{0,3})(\.\d{0,2})?/);
-      this.value = match ? match[0] : '';
-    }
-  });
+  if (field) {                                          // <-- Lägg till denna koll!
+    field.addEventListener('input', function() {
+      this.value = this.value.replace(/[^0-9.,]/g, '');
+      if (this.value.includes(',')) {
+        this.value = this.value.replace(',', '.');
+      }
+      if (field.classList.contains('numeric-hours')) {
+        var match = this.value.match(/^(\d{0,3})(\.\d{0,2})?/);
+        this.value = match ? match[0] : '';
+      }
+    });
+  }
 });
 
 });
